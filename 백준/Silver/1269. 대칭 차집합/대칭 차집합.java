572cda191eba7ee5d8382cf5bf05127b61f6a73d
woong-sung <104243252+woong-sung@users.noreply.github.com>
2022-07-06 16:27:27 +0900

    [Silver III] Title: 대칭 차집합, Time: 708 ms, Memory: 77944 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -7,37 +7,26 @@ import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuffer sb = new StringBuffer();
-		
-		String num = br.readLine();
-		int a = Integer.parseInt(num.split(" ")[0]);
-		int b = Integer.parseInt(num.split(" ")[1]);
-		
-		HashSet<Integer> A = new HashSet<>();
-		HashSet<Integer> B = new HashSet<>();
-		HashSet<Integer> tempA = new HashSet<>();
-		
-		String Aset = br.readLine();
-		
-		String Bset = br.readLine();
-		
-		StringTokenizer stA = new StringTokenizer(Aset," ");
-		StringTokenizer stB = new StringTokenizer(Bset," ");
-		
-		for(int i=0;i<a;i++) {
-			int c = Integer.parseInt(stA.nextToken());
-			A.add(c);
-			tempA.add(c);
-		}
-		for(int i=0;i<b;i++) {
-			B.add(Integer.parseInt(stB.nextToken()));
-		}
-		tempA.removeAll(B);
-		B.removeAll(A);
-		int sum = tempA.size() +B.size();
-		System.out.println(sum); 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+        String num = br.readLine();
+        int a = Integer.parseInt(num.split(" ")[0]);
+        int b = Integer.parseInt(num.split(" ")[1]);
+        HashSet<Integer> set = new HashSet<>();
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        for (int i=0;i<a;i++){
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        String str2 = br.readLine();
+        StringTokenizer st2 = new StringTokenizer(str2," ");
+        for (int i=0;i<b;i++){
+            set.add(Integer.parseInt(st2.nextToken()));
+        }
+        int answer = set.size() - (a + b - set.size());
+
+        System.out.println(answer);
+    }
 }
