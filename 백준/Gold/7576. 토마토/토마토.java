33d0c736c0c16457e1b28d6e5e02e748b12182a8
woong-sung <104243252+woong-sung@users.noreply.github.com>
2022-08-04 00:50:14 +0900

    [Gold V] Title: 토마토, Time: 776 ms, Memory: 147604 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[][] box;
@@ -10,22 +11,26 @@ public class Main {
     static Queue<Integer[]> q;
 
     public static void main(String[] args) throws IOException {
-        final int[] xarr = new int[]{-1, 0, 1, 0};
-        final int[] yarr = new int[]{0, -1, 0, 1};
-
+        //상하좌우 비교를 위한 방향
+        final int[] dx = new int[]{-1, 0, 1, 0};
+        final int[] dy = new int[]{0, -1, 0, 1};
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         //N은 세로 M은 가로
-        String[] MN = br.readLine().split(" ");
-        M = Integer.parseInt(MN[0]);
-        N = Integer.parseInt(MN[1]);
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str);
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
         cnt = 0;
         // box는 창고
         box = new int[N][M];
         q = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            String[] str = br.readLine().split(" ");
+            String str2 = br.readLine();
+            StringTokenizer st2 = new StringTokenizer(str2);
             for (int j = 0; j < M; j++) {
-                box[i][j] = Integer.parseInt(str[j]);
+                box[i][j] = Integer.parseInt(st2.nextToken());
+                // 이미 익어있는(1인) 좌표를 큐에 넣는다.
                 if (box[i][j] == 1) {
                     q.add(new Integer[]{i, j});
                     cnt++;
@@ -36,16 +41,17 @@ public class Main {
             Integer[] A = q.poll();
             int y = A[0];
             int x = A[1];
-
-            //4방향을 확인해서 0이 있으면 q에 해당좌표를 넣는다(1이 됐으므로)
             for (int i = 0; i < 4; i++) {
-                if (find(y + yarr[i], x + xarr[i])) {
-                    q.add(new Integer[]{y + yarr[i], x + xarr[i]});
-                    box[y + yarr[i]][x + xarr[i]] = box[y][x] + 1;
+                if (find(y + dy[i], x + dx[i])) {
+                    //4방향을 확인해서 0이 있으면 q에 해당좌표를 넣는다(1이 됐으므로)
+                    q.add(new Integer[]{y + dy[i], x + dx[i]});
+                    // 그리고 해당칸에 이전칸의 수+1를 넣는다( 날짜표시 )
+                    box[y + dy[i]][x + dx[i]] = box[y][x] + 1;
                 }
             }
 
         }
+        //
         int max = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
@@ -59,7 +65,7 @@ public class Main {
         }
         System.out.println(max - 1);
     }
-
+    // 해당 칸이 경계 안에 있고 0인지 확인
     public static boolean find(int y, int x) {
         if (y < 0 || y >= N || x < 0 || x >= M) return false;
         if (box[y][x] == 0) return true;
