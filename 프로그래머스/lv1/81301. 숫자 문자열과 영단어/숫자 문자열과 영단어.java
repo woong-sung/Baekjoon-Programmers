7d5628b413c54480124e6d0a1f20958c4fc7adc0
조 성웅 <cjswo138@gmail.com>
2022-12-13 16:47:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.41 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,20 +1,19 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        
-	    String str = s;
-	    
-	    str = str.replaceAll("zero","0");
-	    str = str.replaceAll("one","1");
-	    str = str.replaceAll("two","2");
-	    str = str.replaceAll("three","3");
-	    str = str.replaceAll("four","4");
-	    str = str.replaceAll("five","5");
-	    str = str.replaceAll("six","6");
-	    str = str.replaceAll("seven","7");
-	    str = str.replaceAll("eight","8");
-	    str = str.replaceAll("nine","9");
-	    answer = Integer.parseInt(str);
+	    s = s.replaceAll("zero","0")
+            .replaceAll("one","1")
+            .replaceAll("two","2")
+            .replaceAll("three","3")
+            .replaceAll("four","4")
+            .replaceAll("five","5")
+            .replaceAll("six","6")
+            .replaceAll("seven","7")
+            .replaceAll("eight","8")
+            .replaceAll("nine","9")
+            ;
+
+	    answer = Integer.parseInt(s);
 	    return answer;
 
     }
