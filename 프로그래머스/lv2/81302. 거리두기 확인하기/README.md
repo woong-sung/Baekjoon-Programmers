d8e9d0b3fe20d46fe76d6ff3093a1e6b1b03fc65
조 성웅 <cjswo138@gmail.com>
2022-12-18 15:19:43 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.20 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 74.7 MB, 시간: 0.09 ms
+메모리: 77.4 MB, 시간: 0.20 ms
 
 ### 구분
 
--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -3,59 +3,78 @@ import java.util.*;
 class Solution {
     public int[] solution(String[][] places) {
         int[] answer = new int[]{1, 1, 1, 1, 1};
-        int[] dx = new int[]{1, 0, -1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-
         for (int t = 0; t < 5; t++) {
-            String[] room = places[t];
-            boolean[][] visited = new boolean[5][5];
-            //세로
-            for (int y = 0; y < 5; y++) {
-                // 가로
-                for (int x = 0; x < 5; x++) {
-                    if (room[y].charAt(x) == 'P') {
-                        if (answer[t] == 0) break;
-                        visited[y][x] = true;
-                        for (int i = 0; i < 4; i++) {
-                            int ny = y + dy[i];
-                            int nx = x + dx[i];
+            String[] rooms = places[t];
+            List<Coordinate> coordinates = new ArrayList<>();
 
-                            if (!check(room, ny, nx, 0, visited)) {
-                                answer[t] = 0;
-                            }
-                        }
+            for (int i = 0; i < 5; i++) {
+                String room = rooms[i];
+                for (int j = 0; j < 5; j++) {
+                    if (room.charAt(j) == 'P') {
+                        coordinates.add(new Coordinate(i, j));
                     }
-                    if (answer[t] == 0) break;
                 }
-                if (answer[t] == 0) break;
+            }
+
+            for (int i = 0; i < coordinates.size(); i++) {
+                int y = coordinates.get(i).y;
+                int x = coordinates.get(i).x;
+                for (int j = i + 1; j < coordinates.size(); j++) {
+                    int ny = coordinates.get(j).y;
+                    int nx = coordinates.get(j).x;
+
+                    if (!check(rooms, y, ny, x, nx)) {
+                        answer[t] = 0;
+                    }
+                }
             }
         }
-
         return answer;
     }
+    
+    class Coordinate {
+        public int y;
+        public int x;
 
-
-    public static boolean check(String[] room, int y, int x, int cnt, boolean[][] visited) {
-        int[] dx = new int[]{1, 0, -1, 0};
-        int[] dy = new int[]{0, 1, 0, -1};
-        if (cnt == 2) return true;
-        else if (y < 0 || x < 0 || x >= 5 || y >= 5) return true;
-        else if (room[y].charAt(x) == 'X') return true;
-        visited[y][x] = true;
-
-        if (room[y].charAt(x) == 'P') return false;
-        else {
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if (ny < 0 || nx < 0 || nx >= 5 || ny >= 5) continue;
-                if (visited[ny][nx]) continue;
-                visited[ny][nx] = true;
-                if (!check(room, ny, nx, cnt + 1, visited)) return false;
-                visited[ny][nx] = false;
-            }
-
+        public Coordinate(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
-        return true;
+    }
+    
+    public boolean check(String[] rooms, int y, int ny, int x, int nx) {
+        int manhattan = Math.abs(y - ny) + Math.abs(x - nx);
+        if (manhattan <= 1) {
+            return false;
+        } else if (manhattan == 2) {
+            if (y == ny) {
+                if (rooms[y].charAt(Math.min(x, nx) + 1) == 'X') {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else if (x == nx) {
+                if (rooms[Math.min(y, ny) + 1].charAt(x) == 'X') {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                // if (x + 1 > 4 || x - 1 < 0) return true;
+                if (x < nx) {
+                    if (rooms[y].charAt(x + 1) == 'X' && rooms[ny].charAt(nx - 1) == 'X') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (rooms[y].charAt(x - 1) == 'X' && rooms[ny].charAt(nx + 1) == 'X') {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        } else return true;
     }
 }
