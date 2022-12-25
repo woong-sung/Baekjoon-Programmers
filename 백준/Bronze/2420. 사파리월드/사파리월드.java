71a7ee86aabd1382b9e20752b5bc349fa7bde734
조 성웅 <cjswo138@gmail.com>
2022-12-25 23:28:14 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        System.out.println(Math.abs(a-b));
+    }
+}
