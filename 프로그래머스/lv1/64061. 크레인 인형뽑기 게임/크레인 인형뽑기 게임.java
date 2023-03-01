a8bb1c2b1c804053db90292d2110237da3c5ce8f
조 성웅 <cjswo138@gmail.com>
2023-03-01 09:55:41 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.02 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> basket = new Stack<>();
+        Queue<Integer>[] allBoard = new Queue[board.length];
+
+        for (int i = 0; i < board.length; i++) {
+            allBoard[i] = new LinkedList<>();
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[j][i]==0) continue;
+                allBoard[i].add(board[j][i]);
+            }
+        }
+
+        basket.add(allBoard[moves[0]-1].poll());
+        for (int i = 1; i < moves.length; i++) {
+            if (allBoard[moves[i] - 1].isEmpty()) {
+                continue;
+            }
+            int cur = allBoard[moves[i]-1].poll();
+            if (basket.isEmpty()) {
+                basket.add(cur);
+            } else {
+                int basketTop = basket.peek();
+                if (cur == basketTop) {
+                    basket.pop();
+                    answer+=2;
+                } else {
+                    basket.add(cur);
+                }
+            }
+        }
+        return answer;
+    }
+}
