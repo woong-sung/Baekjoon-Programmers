82c1c4c0d446151a2fd7c2aa03dab58eea5b984d
조 성웅 <cjswo138@gmail.com>
2023-03-22 00:57:42 +0900

    [level 3] Title: 표 편집, Time: 922.86 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/81303. 표 편집/표 편집.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+            StringBuilder answer = new StringBuilder();
+
+        int total = n;
+        Stack<Integer> removed = new Stack<>();
+        int cur = k;
+        for (int i = 0; i < cmd.length; i++) {
+            char move = cmd[i].charAt(0);
+
+            if (move == 'D') {
+                int go = Integer.parseInt(cmd[i].split(" ")[1]);
+                cur += go;
+            } else if (move == 'U') {
+                int go = Integer.parseInt(cmd[i].split(" ")[1]);
+                cur -= go;
+            } else if (move == 'C') {
+                total--;
+                removed.add(cur);
+                if (total == cur) cur--;
+            } else if (move == 'Z') {
+                int pre = removed.pop();
+                if (pre <= cur) {
+                    cur++;
+                }
+                total++;
+            }
+        }
+        for (int i = 0; i < total; i++) {
+            answer.append("O");
+        }
+
+        while (!removed.isEmpty()) {
+            int pre = removed.pop();
+            answer.insert(pre, "X");
+        } 
+        return answer.toString();
+    }
+}
