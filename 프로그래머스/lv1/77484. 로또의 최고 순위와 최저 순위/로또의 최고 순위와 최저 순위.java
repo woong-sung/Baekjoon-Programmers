1385026fb6037415462846fd25803c2f337881ce
조 성웅 <cjswo138@gmail.com>
2023-03-22 00:59:20 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int matching = 0;
+        int blank = 0;
+        
+        for(int i = 0; i < 6; i++){
+            if(lottos[i] == 0){
+                blank++;
+            }
+            for(int j = 0; j < 6; j++){    
+                if(lottos[i] == win_nums[j]){
+                    matching++;
+                    break;
+                } 
+            }
+        }
+        int highRank = 7 - (matching + blank);
+        int lowRank = 7 - matching;
+        
+        if (lowRank == 7) lowRank = 6;
+        if (highRank == 7) highRank = 6;
+        
+        
+        int[] answer = new int[]{highRank, lowRank};
+        
+        return answer;
+    }
+}
