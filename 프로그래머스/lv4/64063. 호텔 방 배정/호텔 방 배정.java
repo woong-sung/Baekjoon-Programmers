3c45ba44c62fb4a642f95aa23079fd06345af8ee
조 성웅 <cjswo138@gmail.com>
2023-03-22 01:04:44 +0900

    [level 4] Title: 호텔 방 배정, Time: 332.32 ms, Memory: 156 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    Map<Long, Long> map;
+    public long[] solution(long k, long[] room_number) {
+         map = new HashMap<>();
+        long[] answer = new long[room_number.length];
+
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = run(room_number[i]);;
+        }
+
+        return answer;
+    } 
+
+    public long run(long c) {
+        if (!map.containsKey(c)) {
+            map.put(c, c + 1);
+            return c;
+        } else {
+            long next = run(map.get(c));
+            map.put(c, next);
+            return next;
+        }
+    }
+}
