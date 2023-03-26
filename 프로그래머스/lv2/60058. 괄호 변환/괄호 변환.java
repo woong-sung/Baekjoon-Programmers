1b18d43089a475bafd91ef49dde24389ac0bed38
조 성웅 <cjswo138@gmail.com>
2023-03-26 09:29:32 +0900

    [level 2] Title: 괄호 변환, Time: 6.86 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
@@ -1,57 +1,70 @@
 import java.util.Stack;
 class Solution {
-        public String solution(String p) {
-            if (check(p)) return p;
-            else return dfs(p);
+    public String solution(String p) {
+        if (check(p)){
+            return p;
+        } else {
+            return dfs(p);
         }
+    }
 
-        // 올바른 문자열인지 확인하는 메서드
-        private static boolean check(String s) {
-            Stack<Character> st = new Stack<>();
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (c == '(') {
-                    st.add(c);
-                } else {
-                    if (st.isEmpty()) return false;
-                    st.pop();
-                }
-            }
-            // 빈 문자열인 경우
-            if (!st.isEmpty()) return false;
-            else return true;
+    public String dfs(String s) {
+        // 조건1 : 빈문자열
+        if (s.length() == 0) {
+            return s;
         }
+        // 조건2 : u,v 로 분리
+        String u = "";
+        String v = "";
+        int l = 0;
+        int r = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
 
-        private static String dfs(String s) {
-            // 조건1 : 빈문자열
-            if (s.length() == 0) return s;
-            // 조건2 : u,v 로 분리
-            String u = "", v = "";
-            int cnt1 = 0, cnt2 = 0;
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
+            if (c == '(') l++;
+            else r++;
 
-                if (c == '(') cnt1++;
-                else cnt2++;
-
-                //균형잡힌 문자열일 때 u,v 분리
-                if (((cnt1 > 0) && (cnt2 > 0)) && (cnt1 == cnt2)) {
-                    u = s.substring(0, i + 1);
-                    if (i < s.length() - 1) v = s.substring(i + 1, s.length());
+            //균형잡힌 문자열일 때 u,v 분리
+            if (l == r) {
+                u = s.substring(0, i + 1);
+                if (i < s.length() - 1) {
+                    v = s.substring(i + 1, s.length());
                     break;
                 }
             }
+        }
 
-            // 조건3 : u가 올바른 괄호문자열인지 체크
-            if (check(u)) {
-                return u + dfs(v);
-            } else {
-                // 조건4 : u가 올바른 괄호문자열이 아니면
-                String tmp = "(" + dfs(v) + ")";
-                u = u.substring(1, u.length() - 1);
-                u = u.replace("(", "p").replace(")", "(").replace("p", ")");
-                tmp = tmp + u;
-                return tmp;
-            }
+        // 조건3 : u가 올바른 괄호문자열인지 체크
+        if (check(u)) {
+            return u + dfs(v);
+        } else {
+            // 조건4 : u가 올바른 괄호문자열이 아니면
+            String temp = "(" + dfs(v) + ")";
+            u = u.substring(1, u.length() - 1);
+            u = u.replace("(", "p").replace(")", "(").replace("p", ")");
+            temp = temp + u;
+            return temp;
         }
     }
+
+    // 올바른 괄호문자열인지 확인하는 메서드
+    public boolean check(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.add(c);
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        // 빈 문자열인 경우
+        if (!stack.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+}
