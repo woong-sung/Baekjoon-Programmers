70b12b1291f49c5a830cde7b9ac4c5e09371e2df
조 성웅 <cjswo138@gmail.com>
2023-04-01 21:19:48 +0900

    [Bronze I] Title: 세로읽기, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Character[][] arr = new Character[5][15];
+        for (int i = 0; i < 5; i++) {
+            Arrays.fill(arr[i], ' ');
+            String s = sc.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i]!=' ') sb.append(arr[j][i]);
+            }
+        }
+        System.out.println(sb);
+    }
+}
