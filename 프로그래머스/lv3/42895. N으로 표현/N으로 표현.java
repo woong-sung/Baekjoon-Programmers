dcba156639948d7c7835865efa8513615eb3a19e
조 성웅 <cjswo138@gmail.com>
2023-04-04 10:23:09 +0900

    [level 3] Title: N으로 표현, Time: 21.27 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.java
@@ -1,69 +1,39 @@
-import java.util.*;
-
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
-    static List<Number> list;
-    static boolean[] visit;
-
     public int solution(int N, int number) {
-        int answer = Integer.MAX_VALUE;
-        visit = new boolean[1000001];    
-        list = new ArrayList<>();   // 해당숫자로 만들 수 있는 기본 숫자 리스트 {5,55,555,5555,...}
-        // 리스트 채우기
-        int c = 1;
-        int f = N;
-        while (f < 1000000) {
-            list.add(new Number(f,c++));
-            visit[f] = true;
-            f=f*10+N;
+       int answer = -1;
+        Set<Integer>[] setArr = new Set[9];
+        int t = N;
+        for(int i = 1; i < 9; i++) {
+            setArr[i] = new HashSet<>();
+            setArr[i].add(t);
+            t = t * 10 + N;
         }
-
-        Queue<Number> q = new PriorityQueue<>();  //우선순위 큐로 설정하여 원하는 최솟값찾기
-        q.addAll(list);
-        // 큐에 계산 결과 넣으면서 원하는 값 찾기
-        while(!q.isEmpty()){
-            Number cur = q.poll();
-            visit[cur.num] = true;
-            if (cur.num == number) {
-                if (cur.cnt > 8) return -1;
-                return cur.cnt;
-            }
-            for (int i = 0; i < list.size(); i++) {
-                int next = 0;
-                for (int j = 0; j < 6; j++) {
-                    if (j==5 && cur.num==0) continue;   // 0으로 나눌 수 없음
-                    if (j==3 && list.get(i).num==0) continue;   // 0으로 나눌 수 없음
-                    next = cal(cur.num, list.get(i).num,j); // 계산 결과
-                    if (next>= visit.length || next<0 || visit[next]) continue; // 범위 밖 혹은 이미 방문
-
-                    q.add(new Number(next, cur.cnt + i+1));
+        for(int i = 1; i < 9; i++) {
+            for(int j = 1; j < i; j++) {
+                for(Integer a : setArr[j]) {
+                    for(Integer b : setArr[i - j]) {
+                        setArr[i].add(a + b);
+                        setArr[i].add(a - b);
+                        setArr[i].add(b - a);
+                        setArr[i].add(a * b);
+                        if(b != 0) {
+                            setArr[i].add(a / b);
+                        }
+                        if(a != 0) {
+                            setArr[i].add(b / a);
+                        }
+                    }
                 }
             }
         }
-        return 0;
-    }
-
-    private int cal(int num, int num1, int j) {     // 계산
-        if (j==0) return num + num1;
-        if (j==1) return num - num1;
-        if (j==2) return num * num1;
-        if (j==3) return num / num1;
-        if (j==4) return num1 - num;
-        if (j==5) return num1 / num;
-        return 0;
-    }
-
-    public static class Number implements Comparable<Number>{   // 각 숫자와 만드는데 필요한 횟수를 저장
-        int num;
-        int cnt;
-
-        public Number(int num, int cnt) {
-            this.num = num;
-            this.cnt = cnt;
+        for(int i = 1; i < 9; i++) {
+            if(setArr[i].contains(number)) {
+                answer = i;
+                break;
+            }
         }
-
-        @Override
-        public int compareTo(Number o) {
-            return this.cnt-o.cnt;
-        }
+        return answer;
     }
 }
