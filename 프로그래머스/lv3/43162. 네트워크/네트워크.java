e9df7a1b58eec45e33221a21bad5f29fd9e3e829
조 성웅 <cjswo138@gmail.com>
2023-04-14 00:32:28 +0900

    [level 3] Title: 네트워크, Time: 0.70 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,35 +1,33 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        List<Integer>[] list = new ArrayList[n];
+        int cnt = 0;
+        List<Integer>[] list = new ArrayList[n];	// 각 컴퓨터에 연결된 컴퓨터번호를 저장할 배열
         for (int i = 0; i < n; i++) {
             list[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (computers[i][j] == 1) {
                     list[i].add(j);
                 }
             }
         }
-        boolean[] checked = new boolean[n];
+        boolean[] checked = new boolean[n];	// 방문체크를 할 배열
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            if (checked[i]) continue;
+            if (checked[i]) continue;	// 이미 포함된 컴퓨터면 넘어가기
             q.add(i);
-            answer++;
+            cnt++;	// i번 컴퓨터를 시작으로 하는 새로운 네트워크가 생성
             while (!q.isEmpty()) {
-                int cur = q.poll();
-
+                int cur = q.poll();	// 현재 컴퓨터
                 checked[cur] =true;
-                for (int next : list[cur]) {
+                for (int next : list[cur]) {	// 현재 컴퓨터에 연결된 컴퓨터 모두 체크 
                     if (checked[next]) continue;
                     checked[next] = true;
                     q.add(next);
                 }
             }
         }
-        return answer;
+        return cnt;
     }
 }
