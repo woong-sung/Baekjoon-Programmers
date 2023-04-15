693168e3313318e18f252416cfd3bcb63e361057
조 성웅 <cjswo138@gmail.com>
2023-04-15 12:47:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.50 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -4,30 +4,28 @@ class Solution {
     public int solution(int[][] maps) {
         int[] dy = new int[]{0, 0, 1, -1};
         int[] dx = new int[]{1, -1, 0, 0};
-        boolean[][][] visit = new boolean[maps.length][maps[0].length][4];
+        boolean[][] visit = new boolean[maps.length][maps[0].length];
         int answer = -1;
         Queue<Node> q = new PriorityQueue<>();
         q.add(new Node(0, 0, 1));
         while (!q.isEmpty()) {
             Node cur = q.poll();
-            int cy = cur.y;
-            int cx = cur.x;
-            int cCnt = cur.cnt;
-            if (cy == maps.length - 1 && cx == maps[0].length - 1) {
-                answer = cCnt;
+            if (cur.y == maps.length - 1 && cur.x == maps[0].length - 1) {
+                answer = cur.cnt;
                 break;
             }
             for (int i = 0; i < 4; i++) {
-                int ny = cy + dy[i];
-                int nx = cx + dx[i];
-                if (ny >= maps.length || ny < 0 || nx >= maps[0].length || nx < 0 || maps[ny][nx] == 0 || visit[ny][nx][i]) continue;
-                visit[ny][nx][i] = true;
-                q.add(new Node(ny, nx, cCnt + 1));
+                int ny = cur.y + dy[i];
+                int nx = cur.x + dx[i];
+                if (ny >= maps.length || ny < 0 || nx >= maps[0].length || nx < 0 || maps[ny][nx] == 0 || visit[ny][nx]) continue;
+                visit[ny][nx] = true;
+                q.add(new Node(ny, nx, cur.cnt + 1));
             }
         }
         return answer;
     }
 }
+
 class Node implements Comparable<Node> {
     int y;
     int x;
