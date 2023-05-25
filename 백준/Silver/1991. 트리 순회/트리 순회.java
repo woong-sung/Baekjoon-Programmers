9de9fbc3bee990b8b460f8767d55e4ddf37aee90
조 성웅 <cjswo138@gmail.com>
2023-05-26 00:45:24 +0900

    [Silver I] Title: 트리 순회, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,79 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static List<Node>[] list;
-    static String ans1="",ans2="",ans3 = "";
+    static Node parent = new Node('A', null, null);
+    static StringBuffer[] ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        list = new ArrayList[26];
-        for (int i = 0; i < 26; i++) {
-            list[i] = new ArrayList<>();
-        }
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            char mid = str.charAt(0);
+            char root = str.charAt(0);
             char left = str.charAt(2);
             char right = str.charAt(4);
-            list[mid-'A'].add(new Node(mid, left, right));
+            insert(parent,root,left,right);
         }
-        front(list[0].get(0));
-        System.out.println(ans1);
-        mid(list[0].get(0));
-        System.out.println(ans2);
-        back(list[0].get(0));
-        System.out.println(ans3);
-    }
-    public static void front(Node node){
-        ans1 += node.cur;
-        if (node.leftChild=='.' && node.rightChild=='.') return;
-        if (node.leftChild != '.') {
-            front(list[node.leftChild - 'A'].get(0));
+        ans = new StringBuffer[3];
+        for (int i = 0; i < 3; i++) {
+            ans[i] = new StringBuffer();
         }
-        if (node.rightChild != '.') {
-            front(list[node.rightChild - 'A'].get(0));
+        preOrder(parent);
+        inOrder(parent);
+        postOrder(parent);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(ans[i]);
         }
     }
 
-    public static void mid(Node node) {
-        if (node.leftChild=='.' && node.rightChild=='.') {
-            ans2 += node.cur;
-            return;
+    public static void insert(Node n, char root, char left, char right) {
+        if (n.cur == root) {    // 현재 노드와 입력받은 노드가 같다면 갱신
+            n.leftChild = (left == '.' ? null : new Node(left,null,null));
+            n.rightChild = (right == '.' ? null : new Node(right,null,null));
         }
-        if (node.leftChild != '.') {
-            mid(list[node.leftChild - 'A'].get(0));
+        else {  // 다르다면 자식노드로 파고들어가서 갱신
+            if(n.leftChild != null) insert(n.leftChild, root, left, right);
+            if(n.rightChild != null) insert(n.rightChild, root, left, right);
         }
-        ans2 += node.cur;
-        if (node.rightChild != '.') {
-            mid(list[node.rightChild - 'A'].get(0));
-        }
+    }
+    public static void preOrder(Node node){
+        if (node == null) return;
+        ans[0].append(node.cur);
+        preOrder(node.leftChild);
+        preOrder(node.rightChild);
     }
 
-    public static void back(Node node){
-        if (node.leftChild=='.' && node.rightChild=='.') {
-            ans3 += node.cur;
-            return;
-        }
-        if (node.leftChild != '.') {
-            back(list[node.leftChild - 'A'].get(0));
-        }
-        if (node.rightChild != '.') {
-            back(list[node.rightChild - 'A'].get(0));
-        } else {
-            ans3 += node.cur;
-            return;
-        }
-        ans3 += node.cur;
+    public static void inOrder(Node node) {
+        if (node == null) return;
+        inOrder(node.leftChild);
+        ans[1].append(node.cur);
+        inOrder(node.rightChild);
     }
+
+    public static void postOrder(Node node){
+        if (node == null) return;
+        postOrder(node.leftChild);
+        postOrder(node.rightChild);
+        ans[2].append(node.cur);
+    }
+
     static class Node{
         char cur;
-        char leftChild;
-        char rightChild;
-        public Node(char c, char l, char r){
+        Node leftChild;
+        Node rightChild;
+        public Node(char c, Node l, Node r){
             this.cur = c;
             this.leftChild = l;
             this.rightChild = r;
