57b03db93363e536fca67c944c7065153300a563
조 성웅 <cjswo138@gmail.com>
2023-10-31 22:24:09 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -3,10 +3,8 @@ class Solution {
         int answer = 0;
         int len = number.length;
         for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                if (j==i) continue;
-                for (int k = j; k < len; k++) {
-                    if (k==j) continue;
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
                     if (number[i] + number[j] + number[k]==0){
                         answer++;
                     }
